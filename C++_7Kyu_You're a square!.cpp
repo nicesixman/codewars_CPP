0012ee096592c01de0f724723fc50d2cca6e6291
nicesixman <nicesixman@naver.com>
2018-11-13 21:55:33 +0900

    C++_7Kyu_You're a square!.cpp
    https://www.codewars.com/kata/54c27a33fb7da0db0100040e/solutions/cpp
    math.h의 sqrt(제곱근 구하는 함수)를 쓰면 아주 쉽게 풀리는 문제.

--- C++_7Kyu_You're a square!.cpp
+++ C++_7Kyu_You're a square!.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <Windows.h>
+#include <math.h>
 using namespace std;
+
+bool is_square(int n)
+{
+	double sqrt_result;
+
+	sqrt_result = sqrt(n);
+	if ((int)sqrt_result == sqrt(n))
+		return true;
+	else
+		return false;
+}
 
 int main()
 {
-	cout << "Hello World!\n";
+	int n;
+	cout << "제곱근 판별할 숫자를 입력하세요: ";
+	cin >> n;
+	is_square(n);
 	
 	system("pause");
 	return 0;
