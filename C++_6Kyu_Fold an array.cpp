a5aa9bfda43ab4d2cc75780f085d3deb1f5ffa4d
nicesixman <nicesixman@naver.com>
2018-11-22 23:36:56 +0900

    C++_6Kyu_Fold an array.cpp
    https://www.codewars.com/kata/57ea70aa5500adfe8a000110/train/cpp
    step 상관 없이 1번 접히는 함수는 구현 완료! STL vector 함수가 유용해서 생각보다 빠르게 구현됐다.
    이어서 할 일은 step을 고려하여 여러번 접히는 함수로 구현하는 것.

--- C++_6Kyu_Fold an array.cpp
+++ C++_6Kyu_Fold an array.cpp
@@ -8,7 +8,39 @@ class Kata
 public:
 	vector<int> foldArray(vector<int> array, int runs)
 	{
-		cout << "Hello foldArray!\n";
+		// 캐스팅을 하는 이유는 Warning C4018을 방지하기 위함.
+		vector<int> NewArray(((int)array.capacity()/2)+1);
+
+		// for debug
+		cout << "Original Array(vector): ";
+		for (int d=0; d<(int)array.capacity(); d++)
+			cout << array[d] << " ";
+		cout << "(Capacity: " << array.capacity() << ")\n";
+		cout << "New Array(vector): ";
+		// 원소의 개수가 짝수일 때는 array 벡터의 원소가 모두 접힌다.
+		if ((int)array.capacity() % 2 == 0)
+		{
+			for (int i=0; i<(((int)array.capacity()/2)); i++)
+			{
+				NewArray[i] = array.at(i) + array.back();
+				array.pop_back();
+				cout << NewArray[i] << " ";
+			}
+			cout << "(Capacity: " << (array.capacity() / 2) << ")\n";
+		}
+		// 원소의 개수가 홀수일 때는 array 벡터의 마지막 원소 1개가 남는다.
+		else if ((int)array.capacity() % 2 == 1)
+		{
+			for (int i=0; i<((int)array.capacity()/2); i++)
+			{
+				NewArray[i] = array.at(i) + array.back();
+				array.pop_back();
+				cout << NewArray[i] << " ";
+			}
+			NewArray.push_back(array.back());
+			cout << NewArray.back() << " ";
+			cout << "(Capacity: " << (array.capacity() / 2) + 1 << ")\n";
+		}
 		return vector<int>();
 	}
 };
@@ -18,8 +50,6 @@ int main()
 	// Sample Test #1.
 	vector<int> array = { 1,2,3,4,5 };
 	int runs = 1;
-
-	cout << "Hello World!\n";
 
 	Kata kata;
 	kata.foldArray(array, runs);
